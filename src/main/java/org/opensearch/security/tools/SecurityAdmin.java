f73a26e5b0416ccb18fd78a8f64debbd94b4b893
Craig Perkins <cwperx@amazon.com>
2022-12-01 12:14:35 -0500

    Remove StandardCharsets from FileReader and FileWriter. Constructor is not available in Java 8
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- src/main/java/org/opensearch/security/tools/SecurityAdmin.java
+++ src/main/java/org/opensearch/security/tools/SecurityAdmin.java
@@ -912,7 +912,7 @@ public class SecurityAdmin {
         }
         
         System.out.println("Will retrieve '"+"/" +id+"' into "+filepath+" "+(legacy?"(legacy mode)":""));
-        try (Writer writer = new FileWriter(filepath, StandardCharsets.UTF_8)) {
+        try (Writer writer = new FileWriter(filepath)) {
 
             final GetResponse response = tc.get(new GetRequest(index).type(type).id(id).refresh(true).realtime(false)).actionGet();
 
