8f0c4183aa6f665adfc0fe8a2b8b8360a5b5a834
Craig Perkins <craig5008@gmail.com>
2023-07-22 07:51:47 -0400

    add import
    Signed-off-by: Craig Perkins <craig5008@gmail.com>

--- src/main/java/org/opensearch/security/transport/SecurityInterceptor.java
+++ src/main/java/org/opensearch/security/transport/SecurityInterceptor.java
@@ -30,6 +30,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
