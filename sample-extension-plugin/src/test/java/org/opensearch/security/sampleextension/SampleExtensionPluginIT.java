56b0c36850558bcbe2fa972f57a8ed4056770e8b
Craig Perkins <cwperx@amazon.com>
2024-10-14 16:20:47 -0400

    Test create with 2 different users
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- sample-extension-plugin/src/test/java/org/opensearch/security/sampleextension/SampleExtensionPluginIT.java
+++ sample-extension-plugin/src/test/java/org/opensearch/security/sampleextension/SampleExtensionPluginIT.java
@@ -9,6 +9,8 @@
 package org.opensearch.security.sampleextension;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 
@@ -23,6 +25,13 @@ import org.opensearch.common.xcontent.json.JsonXContent;
 import org.opensearch.core.xcontent.NamedXContentRegistry;
 
 public class SampleExtensionPluginIT extends ODFERestTestCase {
+
+    // @BeforeClass
+    // public static void createTestUsers() throws IOException {
+    // Request createUserRequest = new Request("POST", "/_opendistro/_security/api/internalusers/craig");
+    // createUserRequest.setJsonEntity("{\"password\":\"changeme\",\"roles\":[\"all_access\"]}");
+    // client().performRequest(createUserRequest);
+    // }
 
     @SuppressWarnings("unchecked")
     public void testPluginsAreInstalled() throws IOException {
@@ -44,10 +53,16 @@ public class SampleExtensionPluginIT extends ODFERestTestCase {
     }
 
     public void testCreateSampleResource() throws IOException, InterruptedException {
-        Request createRequest = new Request("POST", "/_plugins/resource_sharing_example/resource");
-        createRequest.setEntity(new StringEntity("{\"name\":\"Craig\"}"));
+        String strongPassword = "myStrongPassword123!";
+        Request createUserRequest = new Request("PUT", "/_opendistro/_security/api/internalusers/craig");
+        createUserRequest.setJsonEntity("{\"password\":\"" + strongPassword + "\",\"opendistro_security_roles\":[\"all_access\"]}");
+        client().performRequest(createUserRequest);
+
         RequestOptions.Builder requestOptions = RequestOptions.DEFAULT.toBuilder();
         requestOptions.setWarningsHandler((warnings) -> false);
+
+        Request createRequest = new Request("POST", "/_plugins/resource_sharing_example/resource");
+        createRequest.setEntity(new StringEntity("{\"name\":\"ExampleResource1\"}"));
         createRequest.setOptions(requestOptions);
         Response response = client().performRequest(createRequest);
         Map<String, String> createResourceResponse = JsonXContent.jsonXContent.createParser(
@@ -57,13 +72,25 @@ public class SampleExtensionPluginIT extends ODFERestTestCase {
         ).mapStrings();
         System.out.println("createResourceResponse: " + createResourceResponse);
 
+        Request createRequest2 = new Request("POST", "/_plugins/resource_sharing_example/resource");
+        createRequest2.setEntity(new StringEntity("{\"name\":\"ExampleResource2\"}"));
+        RequestOptions.Builder requestOptions2 = RequestOptions.DEFAULT.toBuilder();
+        requestOptions2.setWarningsHandler((warnings) -> false);
+        requestOptions2.addHeader("Authorization", Base64.getEncoder().encodeToString(strongPassword.getBytes(StandardCharsets.UTF_8)));
+        createRequest2.setOptions(requestOptions2);
+        Response response2 = client().performRequest(createRequest);
+        Map<String, String> createResourceResponse2 = JsonXContent.jsonXContent.createParser(
+            NamedXContentRegistry.EMPTY,
+            LoggingDeprecationHandler.INSTANCE,
+            response.getEntity().getContent()
+        ).mapStrings();
+        System.out.println("createResourceResponse2: " + createResourceResponse2);
+
         // Sleep to give ResourceSharingListener time to create the .resource-sharing index
         Thread.sleep(1000);
 
         Request listRequest = new Request("GET", "/_plugins/resource_sharing_example/resource");
-        RequestOptions.Builder listRequestOptions = RequestOptions.DEFAULT.toBuilder();
-        requestOptions.setWarningsHandler((warnings) -> false);
-        listRequest.setOptions(listRequestOptions);
+        listRequest.setOptions(requestOptions);
         Response listResponse = client().performRequest(listRequest);
         Map<String, String> listResourceResponse = JsonXContent.jsonXContent.createParser(
             NamedXContentRegistry.EMPTY,
@@ -73,9 +100,7 @@ public class SampleExtensionPluginIT extends ODFERestTestCase {
         System.out.println("listResourceResponse: " + listResourceResponse);
 
         Request resourceSharingRequest = new Request("POST", "/.resource-sharing/_search");
-        RequestOptions.Builder resourceSharingRequestOptions = RequestOptions.DEFAULT.toBuilder();
-        requestOptions.setWarningsHandler((warnings) -> false);
-        resourceSharingRequest.setOptions(resourceSharingRequestOptions);
+        resourceSharingRequest.setOptions(requestOptions);
         Response resourceSharingResponse = adminClient().performRequest(resourceSharingRequest);
         Map<String, Object> resourceSharingResponseMap = JsonXContent.jsonXContent.createParser(
             NamedXContentRegistry.EMPTY,
