c141a6fd6dfc238a56807c08e052e2efa6183368
Craig Perkins <cwperx@amazon.com>
2024-10-21 22:55:18 -0400

    Add integration test to verify plugin installed
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- sample-extension-plugin/src/integrationTest/java/org/opensearch/security/sampleextension/SampleExtensionPluginTests.java
+++ sample-extension-plugin/src/integrationTest/java/org/opensearch/security/sampleextension/SampleExtensionPluginTests.java
@@ -17,46 +17,41 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.opensearch.test.framework.TestSecurityConfig;
-import org.opensearch.test.framework.TestSecurityConfig.Role;
 import org.opensearch.test.framework.cluster.ClusterManager;
 import org.opensearch.test.framework.cluster.LocalCluster;
 import org.opensearch.test.framework.cluster.TestRestClient;
 import org.opensearch.test.framework.cluster.TestRestClient.HttpResponse;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.opensearch.test.framework.TestSecurityConfig.AuthcDomain.AUTHC_HTTPBASIC_INTERNAL;
+import static org.opensearch.test.framework.TestSecurityConfig.User.USER_ADMIN;
 
 @RunWith(com.carrotsearch.randomizedtesting.RandomizedRunner.class)
 @ThreadLeakScope(ThreadLeakScope.Scope.NONE)
 public class SampleExtensionPluginTests {
 
-    protected final static TestSecurityConfig.User USER_SR = new TestSecurityConfig.User("sr_user").roles(
-        new Role("abc_ber").indexPermissions("*").on("*").clusterPermissions("*"),
-        new Role("def_efg").indexPermissions("*").on("*").clusterPermissions("*")
-    );
-
     @ClassRule
     public static LocalCluster cluster = new LocalCluster.Builder().clusterManager(ClusterManager.THREE_CLUSTER_MANAGERS)
+        .plugin(SampleExtensionPlugin.class)
         .anonymousAuth(true)
         .authc(AUTHC_HTTPBASIC_INTERNAL)
-        .users(USER_SR)
+        .users(USER_ADMIN)
         .build();
 
     @Test
     public void testSecurityRoles() throws Exception {
-        try (TestRestClient client = cluster.getRestClient(USER_SR)) {
+        try (TestRestClient client = cluster.getRestClient(USER_ADMIN)) {
             HttpResponse response = client.getAuthInfo();
             response.assertStatusCode(HttpStatus.SC_OK);
 
             // Check username
-            assertThat(response.getTextFromJsonBody("/user_name"), equalTo("sr_user"));
-
-            // Check security roles
-            assertThat(response.getTextFromJsonBody("/roles/0"), equalTo("user_sr_user__abc_ber"));
-            assertThat(response.getTextFromJsonBody("/roles/1"), equalTo("user_sr_user__def_efg"));
-
+            assertThat(response.getTextFromJsonBody("/user_name"), equalTo("admin"));
+            System.out.println("Response: " + response.getBody());
+            HttpResponse pluginsResponse = client.get("_cat/plugins?s=component&h=name,component,version,description");
+            assertThat(pluginsResponse.getBody(), containsString("org.opensearch.security.OpenSearchSecurityPlugin"));
+            assertThat(pluginsResponse.getBody(), containsString("org.opensearch.security.sampleextension.SampleExtensionPlugin"));
         }
     }
 
