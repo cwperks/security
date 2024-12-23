b1c02dd4c15c2175d5f0457d1281ebc5343b9fd3
Craig Perkins <cwperx@amazon.com>
2024-12-23 16:56:54 -0500

    Rename listener
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- spi/src/main/java/org/opensearch/security/spi/ResourceSharingService.java
+++ spi/src/main/java/org/opensearch/security/spi/ResourceSharingService.java
@@ -3,5 +3,5 @@ package org.opensearch.security.spi;
 import org.opensearch.core.action.ActionListener;
 
 public interface ResourceSharingService<T extends Resource> {
-    void isSharedWithCurrentUser(String resourceId, ActionListener<Boolean> resourceSharingListener);
+    void isSharedWithCurrentUser(String resourceId, ActionListener<Boolean> shareListener);
 }
