91e00197cb088fa1533d76cd5fbd15ddc07d35fa
Craig Perkins <cwperx@amazon.com>
2025-04-22 21:45:19 -0400

    Add JSON Array support to webhook sink
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- src/main/java/org/opensearch/security/auditlog/sink/WebhookSink.java
+++ src/main/java/org/opensearch/security/auditlog/sink/WebhookSink.java
@@ -178,6 +178,18 @@ public class WebhookSink extends AuditLogSink {
     }
 
     /**
+     * Transforms an {@link AuditMessage} to JSON Array. By default, all fields are
+     * included in the JSON string. This method can be overridden by subclasses
+     * if a specific JSON format is needed.
+     *
+     * @param msg the AuditMessage to transform
+     * @return the JSON string
+     */
+    protected String formatJsonArray(final AuditMessage msg) {
+        return "[" + msg.toJson() + "]";
+    }
+
+    /**
      * Transforms an {@link AuditMessage} to plain text. This method can be overridden
      * by subclasses if a specific text format is needed.
      *
@@ -262,6 +274,9 @@ public class WebhookSink extends AuditLogSink {
         switch (webhookFormat) {
             case JSON:
                 payload = formatJson(msg);
+                break;
+            case JSON_ARRAY:
+                payload = formatJsonArray(msg);
                 break;
             case TEXT:
                 payload = formatText(msg);
@@ -420,6 +435,7 @@ public class WebhookSink extends AuditLogSink {
         URL_PARAMETER_POST(HttpMethod.POST, ContentType.TEXT_PLAIN),
         TEXT(HttpMethod.POST, ContentType.TEXT_PLAIN),
         JSON(HttpMethod.POST, ContentType.APPLICATION_JSON),
+        JSON_ARRAY(HttpMethod.POST, ContentType.APPLICATION_JSON),
         SLACK(HttpMethod.POST, ContentType.APPLICATION_JSON);
 
         private HttpMethod method;
